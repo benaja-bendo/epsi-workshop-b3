cec04e1abab0d0b5e9978d558702025a1226deb9
Baptiste <chapdan1112@gmail.com>
2023-10-04 09:14:59 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AssociationController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BonPlanController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
