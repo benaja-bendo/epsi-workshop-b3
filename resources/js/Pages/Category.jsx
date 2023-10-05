760ad45c9d2bacd6c7a0e573944b09db4e589140
Rémi Tardif <91246696+Remisterioo@users.noreply.github.com>
2023-10-05 08:19:39 +0200

    ListBonPlan

--- resources/js/Pages/Category.jsx
+++ resources/js/Pages/Category.jsx
@@ -18,10 +18,10 @@ export default function Category(props) {
     <AuthenticatedLayout
             user={auth.user}
             header={<h2
-                className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Catégories</h2>
+                className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"></h2>
                 
             }
- 
+
         >
         <h1>
             Category
